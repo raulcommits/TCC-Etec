701e30207596c0b6be881dda58f09cff20e02428
Jotaramalho <pedroca.2596@gmail.com>
2025-08-25 20:43:34 -0300

    Feita a controller de Paciente

--- API Back-End/src/routes.js
+++ API Back-End/src/routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import usuarioController from "./controllers/usuarioController.js";
 import loginController from "./controllers/loginController.js";
+import pacienteController from "./controllers/pacienteController.js";
 
 const routes = express();
 
 routes.use("/usuario", usuarioController);
 routes.use("/login", loginController);
-
+routes.use("/paciente", pacienteController);
 export default routes;
