adc441e66e48258ef72c422e98928b36f39a0d4a
luizfelipe-gif <luizf.892002@gmail.com>
2025-11-17 16:08:17 -0300

    v4.5.7 -> Melhorias na estilização da págiona de perfil do agente de saúde.

--- Frontend Luiz/VyaSaude/client/src/components/Modal_DetalhesRegistro/Index.jsx
+++ Frontend Luiz/VyaSaude/client/src/components/Modal_DetalhesRegistro/Index.jsx
@@ -85,55 +85,53 @@ export default function Modal_DetalhesRegistro({onClose, registroId}) {
             </div>
 
             <div className="Modal_DetalhesRegistro-elements">
-               <div id="modal_detalhesRegistro">
-                  <span className="d-flex h5 text-success">Dados do Paciente</span>
-                  <div className="grid grid_1">
-                     <TextField variant="outlined" name="nome" value={dadosPaciente.nome} onChange={(e) => handleFormChange(e)} label="Nome do Paciente"></TextField>
-                     <TextField variant="outlined" name="cpf"  value={dadosPaciente.cpf} onChange={(e) => handleFormChange(e)} type="text" label="CPF do Paciente"></TextField>
-                     <TextField variant="outlined" name="sus"  value={dadosPaciente.sus} onChange={(e) => handleFormChange(e)} label="Nº SUS" type="number"></TextField>
-                  </div>
-                  
-                  <hr/>
-                  <span className="d-flex h5 text-success">Endereço</span>
+               <span className="h5 text-success">Dados do Paciente</span>
+               <div className="grid grid_1">
+                  <TextField variant="outlined" name="nome" value={dadosPaciente.nome} onChange={(e) => handleFormChange(e)} label="Nome do Paciente"></TextField>
+                  <TextField variant="outlined" name="cpf"  value={dadosPaciente.cpf} onChange={(e) => handleFormChange(e)} type="text" label="CPF do Paciente"></TextField>
+                  <TextField variant="outlined" name="sus"  value={dadosPaciente.sus} onChange={(e) => handleFormChange(e)} label="Nº SUS" type="number"></TextField>
+               </div>
+               
+               <hr/>
+               <span className="h5 text-success">Endereço</span>
 
-                  <div className="grid grid_2">
-                     <TextField variant="outlined" name="logradouro"    value={dadosEndereco.logradouro} onChange={(e) => handleFormChange(e)} label="Logradouro"></TextField>
-                     <TextField variant="outlined" name="numero"        value={dadosEndereco.numero} onChange={(e) => handleFormChange(e)} label="Número" type="number"></TextField>
-                     <TextField variant="outlined" name="complemento"   value={dadosEndereco.complemento} onChange={(e) => handleFormChange(e)} label="Complemento"></TextField>
-                     <TextField variant="outlined" name="cep"           value={dadosEndereco.cep} onChange={(e) => handleFormChange(e)} label="CEP" type="number"></TextField>
-                  </div>
-                  
-                  <div className="grid grid_3">
-                     <TextField variant="outlined" name="bairro" value={dadosEndereco.bairro} onChange={(e) => handleFormChange(e)} label="Bairro"></TextField>
-                     <TextField variant="outlined" name="cidade" value={dadosEndereco.cidade} onChange={(e) => handleFormChange(e)} label="Cidade"></TextField>
-                     <TextField variant="outlined" name="estado" value={dadosEndereco.estado} onChange={(e) => handleFormChange(e)} label="UF"></TextField>
-                  </div>
+               <div className="grid grid_2">
+                  <TextField variant="outlined" name="logradouro"    value={dadosEndereco.logradouro} onChange={(e) => handleFormChange(e)} label="Logradouro"></TextField>
+                  <TextField variant="outlined" name="numero"        value={dadosEndereco.numero} onChange={(e) => handleFormChange(e)} label="Número" type="number"></TextField>
+                  <TextField variant="outlined" name="complemento"   value={dadosEndereco.complemento} onChange={(e) => handleFormChange(e)} label="Complemento"></TextField>
+                  <TextField variant="outlined" name="cep"           value={dadosEndereco.cep} onChange={(e) => handleFormChange(e)} label="CEP" type="number"></TextField>
+               </div>
+               
+               <div className="grid grid_3">
+                  <TextField variant="outlined" name="bairro" value={dadosEndereco.bairro} onChange={(e) => handleFormChange(e)} label="Bairro"></TextField>
+                  <TextField variant="outlined" name="cidade" value={dadosEndereco.cidade} onChange={(e) => handleFormChange(e)} label="Cidade"></TextField>
+                  <TextField variant="outlined" name="estado" value={dadosEndereco.estado} onChange={(e) => handleFormChange(e)} label="UF"></TextField>
+               </div>
 
-                  <hr/>
-                  <span className="d-flex h5 text-success">Dados do Agente</span>
+               <hr/>
+               <span className="h5 text-success">Dados do Agente</span>
 
-                  <div className="grid grid_4">
-                     <TextField variant="outlined" name="nome_agente"   value={dadosAgente.nome_agente} onChange={(e) => handleFormChange(e)} label="Agente de Saúde"></TextField>
-                     <TextField variant="outlined" disabled name="cns"  value={dadosAgente.cns} onChange={(e) => handleFormChange(e)} label="CNS" type="number"></TextField>
-                     <TextField variant="outlined" name="nome_posto"    value={dadosAgente.posto.nome_posto} onChange={(e) => handleFormChange(e)} label="UBS"></TextField>
-                  </div>
+               <div className="grid grid_4">
+                  <TextField variant="outlined" name="nome_agente"   value={dadosAgente.nome_agente} onChange={(e) => handleFormChange(e)} label="Agente de Saúde"></TextField>
+                  <TextField variant="outlined" disabled name="cns"  value={dadosAgente.cns} onChange={(e) => handleFormChange(e)} label="CNS" type="number"></TextField>
+                  <TextField variant="outlined" name="nome_posto"    value={dadosAgente.posto.nome_posto} onChange={(e) => handleFormChange(e)} label="UBS"></TextField>
+               </div>
 
-                  <hr/>
-                  <span className="d-flex h5 text-success">Sobre a visita</span>
+               <hr/>
+               <span className="h5 text-success">Sobre a visita</span>
 
-                  <div className="grid grid_4">
-                     <TextField variant="outlined" name="motivo"        value={dadosRegistro.motivo} onChange={(e) => handleFormChange(e)} label="Motivo"></TextField>
-                     <TextField variant="outlined" name="desfecho"      value={dadosRegistro.desfecho} onChange={(e) => handleFormChange(e)} label="Desfecho"></TextField>
-                     <TextField variant="outlined" name="data_visita"   value={new Date(dadosRegistro.data_visita).toLocaleString('pt-BR')} onChange={(e) => handleFormChange(e)} label="Data/Hora"></TextField>
-                  </div>
+               <div className="grid grid_4">
+                  <TextField variant="outlined" name="motivo"        value={dadosRegistro.motivo} onChange={(e) => handleFormChange(e)} label="Motivo"></TextField>
+                  <TextField variant="outlined" name="desfecho"      value={dadosRegistro.desfecho} onChange={(e) => handleFormChange(e)} label="Desfecho"></TextField>
+                  <TextField variant="outlined" name="data_visita"   value={new Date(dadosRegistro.data_visita).toLocaleString('pt-BR')} onChange={(e) => handleFormChange(e)} label="Data/Hora"></TextField>
+               </div>
 
-                  <div className="grid">
-                     <TextField variant="outlined" name="descricao" multiline rows={3} value={dadosRegistro.descricao} onChange={(e) => handleFormChange(e)} label="Descrição"></TextField>
-                  </div>
+               <div className="grid">
+                  <TextField variant="outlined" name="descricao" multiline rows={3} value={dadosRegistro.descricao} onChange={(e) => handleFormChange(e)} label="Descrição"></TextField>
+               </div>
 
-                  <div className="modal_buttons">
-                     <Button variant="outline-success" onClick={onClose}>Fechar</Button>
-                  </div>
+               <div className="modal_buttons">
+                  <Button variant="outline-success" onClick={onClose}>Fechar</Button>
                </div>
             </div>
          </div>
--- Frontend Luiz/VyaSaude/client/src/pages/agente/Agente_perfil.jsx
+++ Frontend Luiz/VyaSaude/client/src/pages/agente/Agente_perfil.jsx
@@ -69,7 +69,7 @@ function Agente_perfil() {
          <Header/>
          <Sidenav/>
          <main className="content-pages">
-            <div className="agente-perfil d-block" style={{position: "relative"}}>
+            <div className="agente_perfil-content">
                <div className="title-pages">
                   <svg onClick={() => navigate(-1)} style={{ cursor:"pointer" }} className="align-self-start"
                   viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 10L3.29289 10.7071L2.58579 10L3.29289 9.29289L4 10ZM21 18C21 18.5523 20.5523 19 20 19C19.4477 19 19 18.5523 19 18L21 18ZM8.29289 15.7071L3.29289 10.7071L4.70711 9.29289L9.70711 14.2929L8.29289 15.7071ZM3.29289 9.29289L8.29289 4.29289L9.70711 5.70711L4.70711 10.7071L3.29289 9.29289ZM4 9L14 9L14 11L4 11L4 9ZM21 16L21 18L19 18L19 16L21 16ZM14 9C17.866 9 21 12.134 21 16L19 16C19 13.2386 16.7614 11 14 11L14 9Z" fill="#000000"></path> </g></svg>
@@ -77,44 +77,38 @@ function Agente_perfil() {
                </div>
                {/* <ButtonBack text="Meu Perfil"/> */}
 
-               <div>
-                  <div>
-                     <span className="h4 text-success">Registro</span>
-                  </div>
-
-                  <div className="form-fields-agente">
+               <div className="agente_perfil-elements">
+                  <span className="h4 text-success">Registro</span>
+                  <div className="grid grid_1">
                      <TextField className="width-large disable" variant="outlined" value={dados.nome_agente} label="Nome completo"/>
                      <TextField className="width-medium disable" variant="outlined" value={dados.cpf} label="CPF"/>
                   </div>
 
-                  <div className="form-fields-agente">
+                  <div className="grid grid_2">
                      <TextField className="width-medium disable" variant="outlined" value={dados.email} label="E-mail"/>
                      <TextField className="width-medium" variant="outlined" value={dados.telefone} label="Telefone" />
                      <TextField className="width-medium" variant="outlined" value={dados?.posto?.nome_posto} label="Posto"/>
                   </div>
-               </div>
 
-                        <br/><hr/><br/>
+                  <hr/>
 
-               <div>
                   <span className="h4 text-success">Informações sobre o cadastro</span>
-               </div>
+                  <div className="grid grid_1">
+                     <TextField className="width-large disable" variant="outlined" value={""} label="Cadastrado por" disabled/>
+                     <TextField className="width-medium disable" variant="outlined" value={new Date(dados?.createdAt).toLocaleString('pt-BR')} label="Data e Hora de criação" />
+                  </div>
 
-               <div className="form-fields-agente">
-                  <TextField className="width-large disable" variant="outlined" value={""} label="Cadastrado por" disabled/>
-                  <TextField className="width-medium disable" variant="outlined" value={new Date(dados?.createdAt).toLocaleString('pt-BR')} label="Data e Hora de criação" />
-               </div>
+                  <div className="grid grid_3">
+                     <TextField className="width-small disable" variant="outlined" value={dados.data_admissao} label="Data de Admissão" />
+                     <TextField className="width-small disable" variant="outlined" value={dados?.cbo?.codigo} label="Código CBO" />
+                     <TextField className="width-large disable" variant="outlined" value={dados?.cbo?.descricao} label="Descrição da Atividade" />
+                  </div>
 
-               <div className="form-fields-agente">
-                  <TextField className="width-small disable" variant="outlined" value={dados.data_admissao} label="Data de Admissão" />
-                  <TextField className="width-small disable" variant="outlined" value={dados?.cbo?.codigo} label="Código CBO" />
-                  <TextField className="width-large disable" variant="outlined" value={dados?.cbo?.descricao} label="Descrição da Atividade" />
-               </div>
-
-               {/* Botões pra voltar e alterar cadastro*/}
-               <div className="form-button">
-                  <button className="btn btn-light border-dark border-opacity-75 px-4 py-2" onClick={() => {navigate('/Admin_home')}}>Voltar pra tela inicial</button>
-                  <button form="form-registro" className="btn btn-light border-dark border-opacity-75 px-4 py-2">Alterar Cadastro</button>
+                  {/* Botões pra voltar e alterar cadastro*/}
+                  <div className="form-button">
+                     <button className="btn btn-light border-dark border-opacity-75 px-4 py-2" onClick={() => {navigate('/Admin_home')}}>Voltar pra tela inicial</button>
+                     <button form="form-registro" className="btn btn-light border-dark border-opacity-75 px-4 py-2">Alterar Cadastro</button>
+                  </div>
                </div>
             </div>
          </main>
